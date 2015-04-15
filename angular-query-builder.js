0357b4d1324d3a4340b30957eba0da12ab17222f
shamith <shamith.c@bridge-india.in>
2015-04-15 18:36:26 +0530

    Angular finish

--- angular-query-builder.js
+++ angular-query-builder.js
@@ -2,11 +2,14 @@ var app = angular.module('app', ['ngSanitize', 'queryBuilder']);
 
 
 app.constant('URL', 'data/');
+var spec_id = document.getElementById("myInput").value;
+app.constant('SPEC_ID', spec_id);
+// console.log(spec_id)
 
-app.factory('DataService', function ($http, URL) {
+app.factory('DataService', function ($http, URL, SPEC_ID) {
     var result;
     var getData = function () {
-        return $http.get(URL + 'content.json');
+        return $http.get(URL + 'content.json?spec_id=' + SPEC_ID);
     };
 
     result = getData();
@@ -20,21 +23,34 @@ app.factory('DataService', function ($http, URL) {
 
 app.factory('OpService', function ($http, URL) {
     var result;
-    var getData = function () {
-        return $http.get(URL + 'operator.json');
+    var getData = function (id) {
+        return $http.get(URL + "operator_"+id+".json");
     };
 
-    result = getData();
+    var getData1 = function () {
+        return $http.get(URL + 'op1.json');
+    };
+
+    // result = getData();
 
      return {
-        getData: function(){
-            return result
-        }  
+        getData: function(id){
+            return getData(id)
+        },
+        getData1: function(){
+            return getData1()
+        },
+
     };
 });
 
-app.controller('QueryBuilderCtrl', ['$scope', 'DataService', function ($scope, DataService) {
-    var data = '{ "group": { "operator": "AND", "rules": [ { "condition": "Equl", "field": 4, "data": "1", "$$hashKey": "005" }, { "group": { "operator": "AND", "rules": [ { "condition": "grater than", "field": 5, "data": "232", "$$hashKey": "00C" }, { "condition": "grater than or eql", "field": 4, "data": "123", "$$hashKey": "00F" }, { "group": { "operator": "AND", "rules": [ { "condition": "grater than", "field": 3, "data": "23", "$$hashKey": "00I" }, { "group": { "operator": "OR", "rules": [ { "group": { "operator": "OR", "rules": [ { "condition": "less than", "field": 11, "data": "23", "$$hashKey": "00T" } ] }, "$$hashKey": "00P" } ] }, "$$hashKey": "00L" } ] }, "$$hashKey": "00E" } ] }, "$$hashKey": "008" } ] } }';
+
+app.factory('nukeService', function($http, URL) {
+    
+});
+
+app.controller('QueryBuilderCtrl', ['$scope', 'DataService', '$http' , function ($scope, DataService, $http) {
+    var data = '{ "group": { "operator": "AND", "rules": [ { "condition": "grater than or eql_14", "field": 14, "data": "11", "$$hashKey": "005" }, { "condition": "less than_15", "field": 15, "data": "222", "$$hashKey": "008" }, { "condition": "grater than_14", "field": 14, "data": "3333", "$$hashKey": "00B" }, { "condition": "Not Equl_14", "field": 14, "data": "44444", "$$hashKey": "00E" }, { "group": { "operator": "AND", "rules": [ { "condition": "grater than_16", "field": 16, "data": "111", "$$hashKey": "00X" }, { "condition": "grater than or eql_11", "field": 11, "data": "222", "$$hashKey": "010" }, { "condition": "Equl_11", "field": 11, "data": "333", "$$hashKey": "013" }, { "condition": "grater than_15", "field": 15, "data": "444", "$$hashKey": "016" }, { "group": { "operator": "AND", "rules": [ { "condition": "less than_13", "field": 13, "data": "333", "$$hashKey": "01D" }, { "condition": "grater than_14", "field": 14, "data": "444", "$$hashKey": "01G" }, { "group": { "operator": "AND", "rules": [ { "group": { "operator": "AND", "rules": [ { "condition": "Equl_11", "field": 11, "data": "222", "$$hashKey": "023" }, { "condition": "Equl_11", "field": 11, "data": "444", "$$hashKey": "026" } ] }, "$$hashKey": "01Z" }, { "condition": "Equl_11", "field": 11, "data": "444", "$$hashKey": "029" }, { "condition": "Equl_11", "field": 11, "data": "222", "$$hashKey": "02C" } ] }, "$$hashKey": "01V" } ] }, "$$hashKey": "019" }, { "condition": "less than_11", "field": 11, "data": "123", "$$hashKey": "01J" }, { "condition": "grater than_14", "field": 14, "data": "456", "$$hashKey": "01M" }, { "condition": "less than_15", "field": 15, "data": "666", "$$hashKey": "01P" }, { "condition": "grater than_13", "field": 13, "data": "999", "$$hashKey": "01S" } ] }, "$$hashKey": "00H" }, { "condition": "grater than_14", "field": 14, "data": "7777", "$$hashKey": "00L" }, { "condition": "less than_11", "field": 11, "data": "6666", "$$hashKey": "00O" }, { "condition": "grater than or eql1_13", "field": 13, "data": "77777", "$$hashKey": "00R" }, { "condition": "grater than or eql_16", "field": 16, "data": "77777", "$$hashKey": "00U" } ] } }';
 
     $scope.json = null;
 
@@ -46,6 +62,12 @@ app.controller('QueryBuilderCtrl', ['$scope', 'DataService', function ($scope, D
     $scope.submitForm = function(isValid) {
         if (isValid) {
             alert('our form is amazing');
+            params = {id: 1}
+            $http.post('/api', params)
+                .success(function(){
+                    console.log('Posted Data!!!')
+                });
+
         }
     };
 
@@ -63,32 +85,40 @@ queryBuilder.directive('queryBuilder', ['$compile', 'DataService', 'OpService' ,
         compile: function (element, attrs) {
             var content, directive;
             content = element.contents().remove();
-            console.log(content);
             return function (scope, element, attrs) {
                 scope.operators = [
                     { name: 'AND' },
                     { name: 'OR' }
                 ];
 
+                scope.conditions = {}
+
                 getFields = function () {
-                    DataService.getData().then(function (result) {
+                    DataService.getData().then(function (result) {                        
                         scope.fields = result.data;
+                        angular.forEach(scope.fields, function(item) {
+                            OpService.getData(item.id).then(function (result) {
+                                scope.conditions[item.id] = result.data;
+                            });
+                           // getOp(item.id); 
+                           // console.log(item.id)
+                        })
+                        
                     });
                 };
 
-                getOp = function() {
-                    OpService.getData().then(function (result) {
-                        scope.conditions = result.data;
-                    });
+                getOp = function(id) {
+                    OpService.getData(id).then(function (result) {
+                       scope.conditions[id] = result.data;
+                   });
                 };
 
                 getFields();
-                getOp();
 
                 scope.addCondition = function () {
                     scope.group.rules.push({
-                        condition: 'Equl',
-                        field: 11,
+                        condition: 'Equl_11',
+                        field: scope.fields[0]["id"],
                         data: ''
                     });
                 };
@@ -106,9 +136,9 @@ queryBuilder.directive('queryBuilder', ['$compile', 'DataService', 'OpService' ,
                     });
                 };
 
-                scope.selectOp = function(t) {
-                    OpService.getData().then(function (result) {
-                        scope.conditions = result.data;
+                scope.selectOp = function(t, field_id) {
+                    OpService.getData(field_id).then(function (result) {
+                        scope.conditions[field_id] = result.data;
                     });
                 }
 
